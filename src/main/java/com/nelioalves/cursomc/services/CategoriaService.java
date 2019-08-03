754029a918566ecd33162f3a7f5b85f28dea20b3
Raphael Milani <ramilani@gmail.com>
2019-08-03 16:01:51 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,11 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 	
 	public Categoria insert(Categoria obj) {
