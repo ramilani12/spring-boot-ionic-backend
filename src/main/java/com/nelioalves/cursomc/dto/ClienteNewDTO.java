5004b311d2de22ef86d283522eda5a3a2731c534
Raphael Milani <ramilani@gmail.com>
2019-11-26 01:12:13 -0300

    Adicionado Validator CPF ou CNPJ

--- src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
@@ -2,6 +2,14 @@ package com.nelioalves.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.nelioalves.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	
 	
@@ -9,22 +17,35 @@ public class ClienteNewDTO implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -600487455890189452L;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=120, message="O nome deve conter entre 5 a 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Email(message="Email inválido")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
-	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String logradouro;
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String numero;
-	private String complemento; 
-	private String bairro;
-	private String cep;
 	
+	private String complemento; 
+	
+	private String bairro;
+	@NotEmpty(message="Preenchimento Obrigatório")
+	private String cep;
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
-	
 	private Integer cidadeId;
 	
 	
