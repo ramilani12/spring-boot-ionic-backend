da55cf5d797d6b99af37654de2051d859f2b91e2
ramilani12 <ramilani@gmail.com>
2020-03-03 01:12:41 -0300

    Adicionado JWT

--- src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
@@ -12,6 +12,7 @@ public class MockEmailService extends AbstractEmailService{
 	
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
 	
+
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		
