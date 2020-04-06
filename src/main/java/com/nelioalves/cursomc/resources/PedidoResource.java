a821bf39db99ae6a75ef97b2ef90b7c1f6560fd7
Raphael Milani <ramilani@gmail.com>
2020-04-06 00:39:33 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/nelioalves/cursomc/resources/PedidoResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/PedidoResource.java
@@ -3,15 +3,19 @@ package com.nelioalves.cursomc.resources;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.services.PedidoService;
 
 @RestController
@@ -39,4 +43,19 @@ public class PedidoResource {
 		
 		return ResponseEntity.created(uri).build();
 	}
+	
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<Page<Pedido>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page , 
+			@RequestParam(value="linesPerPage", defaultValue="24")  Integer linerPerPage, 
+			@RequestParam(value="orderBy", defaultValue="instante") String orderBy , 
+			@RequestParam(value="direction", defaultValue="DESC") String direction) {		
+		
+		Page<Pedido> list = service.findPage(page, linerPerPage, orderBy, direction);
+		
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
