043c2695125e2b11511b1f8424b1ea3da8ca8df7
Raphael Milani <ramilani@gmail.com>
2020-04-10 18:35:33 -0300

    Adicionado Endpoint recuperar a senha

--- src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	
 	private static final String[] PUBLIC_MATHERS_POST = { 
-			"/clientes/**"
+			"/clientes/**",
+			"/auth/forgot/**"
 };
 	
 	private static final String[] PUBLIC_MATHERS_GET = { 
